d687ad95cc628ae0471aa48dcccb82378ffabbaa
Sanyog Dani <113448326+SanD204@users.noreply.github.com>
2023-10-23 17:18:12 +0530

    Update project_database.sql

--- project_database.sql
+++ project_database.sql
@@ -18,8 +18,6 @@
 --
 -- Table structure for table `product_info_table`
 --
-
-DROP TABLE IF EXISTS `product_info_table`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!50503 SET character_set_client = utf8mb4 */;
 CREATE TABLE `product_info_table` (
